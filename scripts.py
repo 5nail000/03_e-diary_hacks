df1f6cf163d5f36ff9d64e42425e868925f8d418
5nail000 <105508594+5nail000@users.noreply.github.com>
2023-04-29 05:14:34 +0300

    fixes

--- scripts.py
+++ scripts.py
@@ -7,39 +7,57 @@ from datacenter.models import (
     Lesson
     )
 
+PRAISE_WORDS = ["Молодец!", "Отлично!", "Хорошо!",
+                "Гораздо лучше, чем я ожидал!", "Ты меня приятно удивил!",
+                "Великолепно!", "Прекрасно!", "Ты меня очень обрадовал!",
+                "Именно этого я давно ждал от тебя!",
+                "Сказано здорово – просто и ясно!", "Ты, как всегда, точен!",
+                "Очень хороший ответ!", "Талантливо!",
+                "Ты сегодня прыгнул выше головы!", "Я поражен!",
+                "Уже существенно лучше!", "Потрясающе!", "Замечательно!",
+                "Прекрасное начало!", "Так держать!", "Ты на верном пути!",
+                "Здорово!", "Это как раз то, что нужно!", "Я тобой горжусь!",
+                "С каждым разом у тебя получается всё лучше!",
+                "Мы с тобой не зря поработали!", "Я вижу, как ты стараешься!",
+                "Ты растешь над собой!", "Ты многое сделал, я это вижу!",
+                "Теперь у тебя точно все получится!"]
+
 
 def find_student(name):
-	from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
-	try:
+    try:
         return Schoolkid.objects.get(full_name__contains=name)
-    except ObjectDoesNotExist:
+    except Schoolkid.DoesNotExist:
         print("Ученик не найден")
-    except MultipleObjectsReturned:
+    except Schoolkid.MultipleObjectsReturned:
         print("Нашёл несколько учеников с указанными совпадениями в имени")
 
 
 def fix_marks(kidname):
-    Mark.objects.filter(schoolkid=find_student(kidname), points__lt=4).update(points=5)	
+    Mark.objects.filter(
+        schoolkid=find_student(kidname),
+        points__lt=4
+        ).update(points=5)
 
-		
+
 def remove_chastisements(kidname):
     Chastisement.objects.filter(schoolkid=find_student(kidname)).delete()
-	
+
 
 def create_commendation(kidname, subject_title="Математика"):
-    texts = ["Молодец!","Отлично!","Хорошо!","Гораздо лучше, чем я ожидал!",
-             "Ты меня приятно удивил!","Великолепно!","Прекрасно!","Ты меня очень обрадовал!",
-             "Именно этого я давно ждал от тебя!","Сказано здорово – просто и ясно!","Ты, как всегда, точен!",
-             "Очень хороший ответ!","Талантливо!","Ты сегодня прыгнул выше головы!","Я поражен!",
-             "Уже существенно лучше!","Потрясающе!","Замечательно!","Прекрасное начало!","Так держать!",
-             "Ты на верном пути!","Здорово!","Это как раз то, что нужно!","Я тобой горжусь!",
-             "С каждым разом у тебя получается всё лучше!","Мы с тобой не зря поработали!",
-             "Я вижу, как ты стараешься!","Ты растешь над собой!","Ты многое сделал, я это вижу!",
-             "Теперь у тебя точно все получится!"]
-	lessons = Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=subject_title)
-	rand_text = texts[randrange(len(texts))]
-    rand_num = randrange(len(lessons))
-	lesson_date = lessons[rand_num].date
-	lesson_teacher = lessons[rand_num].teacher
-	lesson_subject = lessons[rand_num].subject
-	Commendation.objects.create(text=rand_text, schoolkid=find_student(kidname), created=lesson_date, subject=lesson_subject, teacher=lesson_teacher)
+    schoolkid = find_student(kidname)
+    lesson = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title=subject_title
+        ).order_by('-date').first()
+    rand_text = PRAISE_WORDS[randrange(len(PRAISE_WORDS))]
+    lesson_date = lesson.date
+    lesson_teacher = lesson.teacher
+    lesson_subject = lesson.subject
+    Commendation.objects.create(
+        text=rand_text,
+        schoolkid=schoolkid,
+        created=lesson_date,
+        subject=lesson_subject,
+        teacher=lesson_teacher
+        )
